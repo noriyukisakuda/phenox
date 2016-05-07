34a2f800fcb73617a14cafe0172426245f38ab1f
Takahiro Miki <takahiro.miki1992@gmail.com>
2016-05-07 13:56:48 +0900

    fixed the problem of direction misrecognition

--- boundary/boundary_detector.h
+++ boundary/boundary_detector.h
@@ -40,6 +40,8 @@ public:
     void detect_edge(Mat* in, Mat* out, Mat* edge);
     // calculate center of gravity from binary image
     bool calc_g(Mat *src, Vector2f *g, Vector2f center, int range);
+    // calculate the number of outside color near to the norm direction
+    int count_direction(Mat *out, Vector2f center, Vector2f norm, int range);
     // use HoughLineP and kmeans to detect two lines from edge image
     int findLine(Mat *src, Mat *in1_image, Vector2f *norm_start1, Vector2f *norm1, Vector2f *norm_start2, Vector2f *norm2);
     
