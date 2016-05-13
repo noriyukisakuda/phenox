8e92cddddd893a7220868888078a55511a75514c
Takahiro Miki <takahiro.miki1992@gmail.com>
2016-05-13 15:33:38 +0900

    delete CMakelists, maker.sh

--- merged/maker.sh
+++
@@ -1,7 +0,0 @@
-export CC=arm-linux-gnueabihf-gcc
-export CXX=arm-linux-gnueabihf-g++
-export AR=arm-linux-gnueabihf-ar
-mkdir build
-cd build
-cmake ../
-cmake --build . --config Release --target install
